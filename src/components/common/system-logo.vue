14b413d8a97a7a9281ef71eb5de56df4433f454e
huyuan <huguangcan@lakala.com>
2026-07-19 11:26:07 +0800

    chore: initial commit

---
+++ src/components/common/system-logo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app-logo">
+    <SvgIcon local-icon="logo" class="logo-icon size-32px!" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.app-logo {
+  :deep(.logo-icon) {
+    color: rgb(var(--primary-color));
+  }
+}
+</style>
