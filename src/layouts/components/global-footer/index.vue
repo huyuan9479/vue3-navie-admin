f3f2d249fb3c7b0dff22cbf8f0014ca9bb57423a
huyuan <huguangcan@lakala.com>
2026-08-02 20:47:19 +0800

    feat:update route

--- src/layouts/components/global-footer/index.vue
+++ src/layouts/components/global-footer/index.vue
@@ -5,7 +5,7 @@ defineOptions({
 </script>
 
 <template>
-  <DarkModeContainer class="h-full flex-center">
+  <DarkModeContainer class="h-full flex-center text-12px">
     <a href="" target="_blank" rel="noopener noreferrer">Copyright MIT © 2021 Vue3NavieAdmin</a>
   </DarkModeContainer>
 </template>
